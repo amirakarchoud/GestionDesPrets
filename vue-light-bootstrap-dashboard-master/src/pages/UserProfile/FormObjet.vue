5323c15b711f43a61edcf1505715e0b61a8c8080
amirakarchoud <48051410+amirakarchoud@users.noreply.github.com>
2023-02-27 18:31:38 +0100

    choix groupe+notif ajout objet

--- vue-light-bootstrap-dashboard-master/src/pages/UserProfile/FormObjet.vue
+++ vue-light-bootstrap-dashboard-master/src/pages/UserProfile/FormObjet.vue
@@ -11,10 +11,27 @@
           <base-input type="text"
                     label="Label"
                     placeholder="Nom objet"
-                    v-model="data.label">
+                    v-model="data.label" style="width:280px;">
           </base-input>
         </div>
         
+      </div>
+
+
+
+
+      <div class="row">
+        <div class="col-md-6" >
+        <label for="group">Groupe </label>
+         <br>
+          <select v-model="group" style="width:280px; height:40px; border:1px solid #d8e1e6;">
+          <option disabled value="">Veuillez selectionner un groupe</option>
+        <option v-for="option in optionsG" :value="option.value">
+             {{ option.text }}
+         </option>
+          </select>
+        </div>
+       
       </div>
 
 
@@ -42,7 +59,7 @@
       
        
       <div class="text-center">
-        <button type="submit" class="btn btn-info btn-fill float-right" @click.prevent="created">
+        <button type="submit" class="btn btn-info btn-fill float-right" @click.prevent="created" :disabled="hasEmptyRequiredFields">
           Ajouter Un Objet
         </button>
         <router-link to="/admin/objet"> 
@@ -56,6 +73,7 @@
 </template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
+  import Notifications from 'vue-notification'
 
   export default {
     components: {
@@ -69,25 +87,55 @@
           borrowed:false
           
         },
-        user: {
-          company: '',
-          username: 'michael23',
-          email: '',
-          firstName: 'Mike',
-          lastName: 'Andrew',
-          address: 'Melbourne, Australia',
-          city: 'melbourne',
-          country: 'Australia',
-          postalCode: '',
-          aboutMe: `Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.`
-        },
+        notifications: {
+          success:{
+          title: 'Objet a été ajouté avec succès!',
+         
+          type: 'success',
+          duration: 3000,
+          position: 'top-right',
+          color: '#ffffff'
+          },
+          failure:{
+           
+          title: 'Erreur!',
+          
+          type: 'warning',
+          duration: 3000,
+          position: 'top-right',
+          color: '#ffffdf'
+          
+          }
+        
+      },
+        types:[],
+        group:'',
        
       selectedT: 'A',
       optionsT: [
         
+      ],
+      optionsG: [
+        
       ]
       }
     },
+    watch: {
+  'group'(newValue, oldValue) {
+    console.log("watch");
+    if (newValue !== oldValue) {
+      console.log("watch in");
+      this.getTypes(newValue);
+      console.log("watch after");
+      console.log(this.optionsT);
+    }
+  }
+},
+computed: {
+    hasEmptyRequiredFields() {
+      return !this.data.label || !this.data.type;
+    }
+  },
     methods: {
       updateProfile () {
         alert('Your data: ' + JSON.stringify(this.user))
@@ -115,18 +163,46 @@
       if (!response.ok) {
         // get error message from body or default to response status
         const error = (data && data.message) || response.status;
+        
         return Promise.reject(error);
       }
 
       this.postId = data.id;
+
+      this.$notify(this.notifications.success);
+      this.$router.push('/admin/objet');
     })
     .catch(error => {
       this.errorMessage = error;
+      this.$notify(this.notifications.failure);
       console.error('There was an error!', error);
     });
 },
 
-getTypes () { 
+getTypes (id) { 
+  console.log("types");
+
+  console.log(this.types);
+   
+       this.optionsT=[];
+       
+       
+      this.optionsT= this.types.filter(o=>o.group==id).map(o => {
+  return {
+    text: o.label,
+    value: o._id
+  };
+          });
+
+    
+    
+    
+    },
+
+
+
+
+    getTypesAll () { 
       this.responseAvailable = false;
 
       fetch("http://localhost:3000/objectType", {
@@ -145,20 +221,45 @@ getTypes () {
         return Promise.reject(error);
       }
       this.responseAvailable=true;
-      this.optionsT= data.map(o => {
+      this.types= data;
+
+    })
+    .catch(error => {
+      this.errorMessage = error;
+      console.error("There was an error!", error);
+    });
+    },
+
+    
+
+
+
+
+    getGroupes () { 
+      this.responseAvailable = false;
+
+      fetch("http://localhost:3000/objectGroup", {
+    "method": "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+})
+.then(async response => {
+      const data = await response.json();
+
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response statusText
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+      this.responseAvailable=true;
+      this.optionsG= data.map(o => {
   return {
     text: o.label,
     value: o._id
   };
           });
-
-     /*  console.log(data);
-      for (let i = 0; i < data.length; i++) {
-  
-   this.optionsT[i].value = data[i]._id;
-   this.optionsT[i].text = data[i].label;
-
-  console.log( data[i].label , data[i]._id); }*/
 
     })
     .catch(error => {
@@ -171,7 +272,9 @@ getTypes () {
 
 beforeMount(){
   //this.test();
-   this.getTypes();
+  this.getTypesAll ()
+  this.getGroupes();
+  // this.getTypes(id);
  },
 
   }
